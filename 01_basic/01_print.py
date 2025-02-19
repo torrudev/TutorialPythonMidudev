e2cf86a2c38c643d28b0794fedc9ab044db561a3
torrudev <jesustorru.dev@gmail.com>
2025-02-19 15:40:49 +0100

    Agregar leccion print(), primer commit del proyecto

---
+++ 01_basic/01_print.py
@@ -0,0 +1,14 @@
+###
+#01 - print()
+#print() es el módulo que permite imprimir en consola
+#lo vamos a usar continuamente
+###
+print("Primera impresión")
+print('También funciona con comillas simples')
+
+print("Impresión","de","valores","distintos")
+
+print("Cambiar","separador","frase", sep = "_")
+
+print("Cambiar el final de este primer print()", end = ".")
+print("Esto es un segundo print()")
