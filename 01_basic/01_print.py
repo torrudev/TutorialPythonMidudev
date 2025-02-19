61bcf8b76c7083120ef7599f8cbadb3b8436ab90
torrudev <jesustorru.dev@gmail.com>
2025-02-19 17:43:01 +0100

    Agregar leccion types y agregar impresión int en leccion print

--- 01_basic/01_print.py
+++ 01_basic/01_print.py
@@ -3,6 +3,7 @@
 #print() es el módulo que permite imprimir en consola
 #lo vamos a usar continuamente
 ###
+
 print("Primera impresión")
 print('También funciona con comillas simples')
 
@@ -12,3 +13,5 @@ print("Cambiar","separador","frase", sep = "_")
 
 print("Cambiar el final de este primer print()", end = ".")
 print("Esto es un segundo print()")
+
+print(73)
---
+++ 01_basic/02_types.py
@@ -0,0 +1,32 @@
+###
+#01 - types()
+#Tipos de datos en python:
+#int, float, complex, str, bool, NoneType, list, tuple, dict, set, range...
+###
+
+print("int:")
+print(10, type(10))
+print(0, type(0))
+print(-2, type(-2))
+
+print("float:")
+print(3.14, type(3.14))
+print(-2.07, type(-2.07))
+print(1e4, type(1e4))
+
+print("complex:")
+print(1 + 2j, type(1 + 2j))
+
+print("str:")
+print("cadena", type("cadena"))
+print("", type(""))
+print("123", type("123"))
+print("""multilinea""", type("""multilinea"""))
+
+print("bool:")
+print(True, type(True))
+print(False, type(False))
+print(1 < 2, type(1 < 2))
+
+print("NoneType:")
+print(None, type(None))
